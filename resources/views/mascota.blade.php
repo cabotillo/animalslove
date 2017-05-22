fbbf7a6f209812dca2f5d8023f618df89350226c
Alberto Cabot <albertogarciacabot@gmail.com>
2017-05-22 19:35:42 +0200

    Update mascota.blade.php

--- resources/views/mascota.blade.php
+++ resources/views/mascota.blade.php
@@ -35,10 +35,17 @@
                         <a href="{{route('imagenes',$mascota->id)}}"><input type="button" class="btn btn-primary" value="Añadir Fotos"></a>
                     @endif
                 @endif
-                <p>{{$mascota->animal}}</p>
-                <p>{{$mascota->raza}}</p>
-                <p>{{$mascota->genero}}</p>
-                <p>Pertenece a <a href="{{'../perfil/',$usuario->id}}{{$usuario->login}}">{{$usuario->login}}</a></p>
+                 <div class="input-group">
+                        <span class="input-group-addon">{{$icono}}</span>
+                        <input type="text" class="form-control" aria-label="Raza" value="{{$mascota->raza}}" disabled>
+                        <span class="input-group-addon">{{$genero}}</span>
+                    </div>
+                    <a href="{{'../perfil/',$usuario->id}}{{$usuario->login}}">
+                    <div class="input-group input-group-lg" style="min-height: 35px;margin-top: 10px">
+                        <span class="input-group-addon">&#128513;</span>
+                        <p style="min-height: 30px;color: #2a88bd" class="form-control">{{$usuario->login}}</p>
+                    </div>
+                    </a>
             </div>
         </div>
     @if(!empty($publicaciones->first()))
